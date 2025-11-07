2b559ffe36684bb3afbf1cf849671cefcc439106
Jelson Araújo <jelsonmatheus200f@gmail.com>
2025-11-07 08:45:19 -0300

    feat: adiciona template padrão

--- application/config/urls.py
+++ application/config/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from concursoapp import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.HomeView.as_view(), name='index'),
 ]
